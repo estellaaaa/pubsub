52a908928e76c92524de44460fdf75b95bbdc9cf
estellaaaa <estella.kinzel@gmail.com>
2024-05-08 17:56:01 +0200

    Struktur

---
+++ pubsub.py
@@ -0,0 +1,22 @@
+class Publisher:
+    def __init__(self):
+        pass
+
+
+
+class Subscriber:
+    def __init__(self):
+        pass
+
+
+
+class Broker:
+     def __init__(self):
+        pass
+
+
+class Message:
+    def __init__(self, topic):
+        self.topic = topic
+
+

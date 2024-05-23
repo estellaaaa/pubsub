c6cd9beca885e7b76bd9348e31e38fc6cebeafed
abductedRhino <avocadorable@mailbox.org>
2024-05-23 23:57:01 +0200

    unsub

--- subscriber.py
+++ subscriber.py
@@ -8,8 +8,10 @@ class Subscriber:
     def __init__(self, name, broker_addr):
         self.name = name
         self.broker_addr = broker_addr
-        
+        self.subscribed = False
+
     def subscribe(self, topic, port):
+        self.subscribed = True
         with socket.create_connection(self.broker_addr, source_address=('127.0.0.1', port)) as connection:
             message = json.dumps(topic)
             connection.sendall(message.encode())
@@ -17,11 +19,13 @@ class Subscriber:
 
     def run(self, topic, port):
         with socket.create_server(('127.0.0.1', port)) as server:
-            while True:
+            while self.subscribed:
                 print(f"SUBSCRIBER: Server listening on 127.0.0.1:{port}")
                 conn, addr = server.accept()
                 print(f"SUBSCRIBER: Connected by {addr}")
                 incoming_message = json.loads(conn.recv(1024).decode())
+                if incoming_message == 'UNSUBSCRIBE_ALL':
+                    self.subscribed = False
                 print(f'HEY SUBSCRIBER {self.name}:', incoming_message)
                 conn.close()
         #self.subscribe(topic, port)

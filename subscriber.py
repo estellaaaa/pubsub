1f9724c94611fa7377e7df5421b8e57822683684
abductedRhino <avocadorable@mailbox.org>
2024-05-23 22:07:14 +0200

    add threading to subscriber

--- subscriber.py
+++ subscriber.py
@@ -1,26 +1,28 @@
 import json
 import socket
 import sys
+import threading
 
 
 class Subscriber:
     def __init__(self, broker_addr, HOST, PORT):
         self.addr = (HOST, PORT)
         self.broker_addr = broker_addr
-
+        
     def subscribe(self, topic, callback):
         with socket.create_connection(self.broker_addr) as connection:
             message = json.dumps(topic)
             connection.sendall(message.encode())
+            threading.Thread(target=self.run).start()
 
     def run(self):
         with socket.create_server(self.addr) as server:
-            while True:
-                print(f"Server listening on {self.addr[0]}:{self.addr[1]}")
-                conn, addr = server.accept()
-                print(f"Connected by {addr}")
-                incoming_message = json.load(conn.recv(1024).decode())
-                print('SUBSCRIBER:', incoming_message)
+            print(f"SUBSCRIBER: Server listening on {self.addr[0]}:{self.addr[1]}")
+            conn, addr = server.accept()
+            print(f"SUBSCRIBER: Connected by {addr}")
+            incoming_message = json.load(conn.recv(1024).decode())
+            print('SUBSCRIBER:', incoming_message)
+            # threading.Thread(target=self.run).start()
 
 
 if __name__ == '__main__':

b56237f3d2fdbcb5f2d7c2cd5e8c42523865e87a
abductedRhino <avocadorable@mailbox.org>
2024-05-23 23:13:31 +0200

    add log

--- publisher.py
+++ publisher.py
@@ -1,12 +1,14 @@
 import json
 import socket
 
+
 class Publisher:
-    def __init__(self, broker_addr):
+    def __init__(self, name, broker_addr):
+        self.name = name
         self.broker_addr = broker_addr
 
     def publish(self, post, topic):
+        print(f'PUBLISHER {self.name}:', post, topic)
         with socket.create_connection(self.broker_addr) as connection:
             message = json.dumps([post, topic])
             connection.sendall(message.encode())
-        

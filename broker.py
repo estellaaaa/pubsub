378cac455795f02602d645e343735539bb67e32b
estellaaaa <estella.kinzel@gmail.com>
2024-05-23 21:58:52 +0200

    change delegate method

--- broker.py
+++ broker.py
@@ -15,13 +15,13 @@ class Broker:
                 print(f"Server listening on {broker_host}:{broker_port}")
                 conn, addr = server.accept()
                 print(f"Connected by {addr}")
-                incoming_message = json.load(conn.recv(1024).decode())
+                incoming_message = json.loads(conn.recv(1024).decode())
                 if type(incoming_message) == str:
                     print('subscribing')
                     threading.Thread(target=self.register2, args=(incoming_message, conn, addr)).start()
                 else:
                     print('publishing')
-                    # threading.Thread(target=self.register2, args=(conn, addr)).start()
+                    threading.Thread(target=self.delegate, args=(incoming_message, conn, addr)).start()
                 print(self.subs)
 
     def register(self, callback, topic):
@@ -31,13 +31,17 @@ class Broker:
         except KeyError:
             self.subs[topic] = [callback]
 
-    def delegate(self, message, topic):
+    def delegate(self, incoming_message, conn, addr):
+        conn.close()
         try:
+            message, topic = incoming_message
             callbacks = self.subs[topic]
         except KeyError:
             return
-        for call in callbacks:
-            call(message)
+        for addr in callbacks:
+            with socket.create_connection(addr) as connection:
+                message = json.dumps(message)
+                connection.sendall(message.encode())
 
     def register2(self, incoming_message, conn, addr):
         topic = incoming_message

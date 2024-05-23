63726415c4af844f342ceec05fdc1a9183bf0884
estellaaaa <estella.kinzel@gmail.com>
2024-05-23 23:35:52 +0200

    wahhhhh

--- pubsub.py
+++ pubsub.py
@@ -8,14 +8,14 @@ import subprocess
 # broker = Broker()
 
 print(f"meow")
-sub1 = Subscriber(('127.0.0.1', 11343))
-sub2 = Subscriber(('127.0.0.1', 11343))
-sub3 = Subscriber(('127.0.0.1', 11343))
+sub1 = Subscriber('Maria', ('127.0.0.1', 11343))
+sub2 = Subscriber('Garfield', ('127.0.0.1', 11343))
+sub3 = Subscriber('Leo', ('127.0.0.1', 11343))
 
-pub1 = Publisher(('127.0.0.1', 11343))
-pub2 = Publisher(('127.0.0.1', 11343))
+pub1 = Publisher('Spiegel', ('127.0.0.1', 11343))
+pub2 = Publisher('BILD', ('127.0.0.1', 11343))
 
-sub1.subscribe('electronics', port=13338)
+sub1.subscribe('electronics', port=13948)
 sub1.subscribe('environment', port=13425)
 
 sub2.subscribe('electronics', port=25843)
@@ -26,6 +26,7 @@ pub1.publish('a new iphone was announced!!', 'electronics')
 pub1.publish('the EM 2024 is starting soon!', 'sports')
 pub2.publish('wow leon musk got richer lol!', 'environment')
 pub1.publish('aslkjdflkdsaf', 'asdf')
+pub2.publish('new ipad coming, get your money ready', 'electronics')
 
 
 # class Message:
--- subscriber.py
+++ subscriber.py
@@ -5,23 +5,24 @@ import threading
 
 
 class Subscriber:
-    def __init__(self, broker_addr):
+    def __init__(self, name, broker_addr):
+        self.name = name
         self.broker_addr = broker_addr
         
     def subscribe(self, topic, port):
         with socket.create_connection(self.broker_addr, source_address=('127.0.0.1', port)) as connection:
             message = json.dumps(topic)
             connection.sendall(message.encode())
-        threading.Thread(target=self.run, args=(port,)).start()
+        threading.Thread(target=self.run, args=(topic, port)).start()
 
-    def run(self, port):
+    def run(self, topic, port):
         with socket.create_server(('127.0.0.1', port)) as server:
             print(f"SUBSCRIBER: Server listening on 127.0.0.1:{port}")
             conn, addr = server.accept()
             print(f"SUBSCRIBER: Connected by {addr}")
             incoming_message = json.loads(conn.recv(1024).decode())
-            print('SUBSCRIBER:', incoming_message)
-            #threading.Thread(target=self.run).start()
+            print(f'HEY SUBSCRIBER {self.name}:', incoming_message)
+        self.subscribe(topic, port)
 
 
 if __name__ == '__main__':

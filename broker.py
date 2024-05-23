1bedaa45100fd19f43c4d60ce875c84045e2de7f
estellaaaa <estella.kinzel@gmail.com>
2024-05-23 21:10:42 +0200

    finishing broker?

--- broker.py
+++ broker.py
@@ -1,18 +1,21 @@
 import socket
+import threading
 
 broker_host = '127.0.0.1'
 broker_port = 11343
 
 class Broker:
-    def __init__(self):
-        # self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # self.server.bind((HOST, PORT))
-        # self.server.listen(10)
-        with socket.create_server((broker_host, broker_port)) as server:
-            print(f"Server listening on {broker_host}:{broker_port}")
-            self.conn, self.addr = server.accept()
-            print(f"Connected by {self.addr}")
+    def __init__(self):       
         self.subs = {}
+
+    def run(self):
+        with socket.create_server((broker_host, broker_port)) as server:
+            while True:
+                print(f"Server listening on {broker_host}:{broker_port}")
+                conn, addr = server.accept()
+                print(f"Connected by {addr}")
+                threading.Thread(target=self.register, args=(conn, addr)).start()
+                print(self.subs)
 
     def register(self, callback, topic):
         try:
@@ -28,5 +31,18 @@ class Broker:
             return
         for call in callbacks:
             call(message)
+
+    def register2(self, conn, addr):
+        topic = conn.recv(1024).decode()
+        try:
+            topic = self.subs[topic]
+            topic.append(addr)
+        except KeyError:
+            self.subs[topic] = [addr]
+        conn.close()
+
+if __name__ == '__main__':
+    broker = Broker()
+    broker.run()
 
 #broker = Broker()
--- pubsub.py
+++ pubsub.py
@@ -12,7 +12,7 @@ sub1 = Subscriber(('127.0.0.1', 11343), '127.0.0.1', 18495)
 sub2 = Subscriber(('127.0.0.1', 11343), '127.0.0.1', 13549)
 sub3 = Subscriber(('127.0.0.1', 11343), '127.0.0.1', 27872)
 
-# pub1 = Publisher(broker)
+pub1 = Publisher(broker)
 # pub2 = Publisher(broker)
 
 sub1.subscribe('electronics', callback=lambda x: print('sub1', x))
@@ -21,8 +21,8 @@ sub1.subscribe('environment', callback=lambda x: print('sub1', x))
 sub2.subscribe('electronics', callback=lambda x: print('sub2', x))
 sub3.subscribe('sports', callback=lambda x: print('sub3', x))
 
-# pub1.publish('a new iphone was announced!!', 'electronics')
-# pub1.publish('aslkjdflkdsaf', 'asdf')
+pub1.publish('a new iphone was announced!!', 'electronics')
+pub1.publish('aslkjdflkdsaf', 'asdf')
 
 
 # class Message:
